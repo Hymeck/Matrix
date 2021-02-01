0fc312c267c63c122f12825b67e72bf6a7cd82f1
Hymeck <noonimf@gmail.com>
2021-02-02 01:27:10 +0300

    add algo's prototype

---
+++ src/Matrix/Demo/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open Library
+open MathNet.Numerics.LinearAlgebra
+
+[<EntryPoint>]
+let main argv =
+    let reversedMatrix = matrix [[1.0; 1.0; 0.0]
+                                 [0.0; 1.0; 0.0]
+                                 [0.0; 0.0; 1.0]]
+    let x = vector [1.0; 0.0; 1.0]
+    let i = 3 - 1
+    let l = Util.multiplyByVector (reversedMatrix, x) // 1.
+    let check = Util.check (l, i)
+    let result =
+        if check.IsSome
+        then
+            let wavedL = Util.setMinusOneAt (x, i) // 2
+            let cuppedL = Util.multiply (l.At i) wavedL // 3
+            let almostE = Util.almostIdentityMatrix i cuppedL // 4
+            let result = Util.finalMultiply almostE reversedMatrix // 5
+            result.ToMatrixString()
+        else "Матрица А с крышкой не обратима!"
+    Console.WriteLine(result)
+    0
--- src/Matrix/Library/Library.fs
+++ src/Matrix/Library/Library.fs
@@ -1,5 +1,34 @@
 ﻿namespace Library
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open MathNet.Numerics.LinearAlgebra
+
+module Util =
+    let minusOne = -1
+    // 1.
+    let multiplyByVector (A: Matrix<double>, x: Vector<double>) =
+        A * x
+    
+    // 2.0
+    let check (x: Vector<double>, i: int) =
+        if x.At i = double 0 then None else Some(x)
+    
+    // 2.1
+    let setMinusOneAt (x: Vector<double>, i: int) =
+        let newX = x.Clone()
+        newX.At (i, -1.0)
+        newX
+    
+    // 3.
+    let multiply (multiplier: double) (x: Vector<double>) =
+        (double (-1) / multiplier) * x
+    
+    // 4.
+    let almostIdentityMatrix (column: int) (x:Vector<double>) =
+        let mutable identityMatrix = Matrix<double>.Build.Dense(x.Count, x.Count)
+        (DiagonalMatrix.identity<double> x.Count).CopyTo(identityMatrix)
+        identityMatrix.SetColumn (column, x)
+        identityMatrix
+    
+    // 5.
+    let finalMultiply (Q: Matrix<double>) (A: Matrix<double>)=
+        Q * A
