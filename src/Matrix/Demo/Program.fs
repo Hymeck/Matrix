2bb91c3a702452b9ac2f1c813229ddd63bedf457
Hymeck <noonimf@gmail.com>
2021-02-02 01:53:29 +0300

    close util fs; add Option<Matrix>

--- src/Matrix/Demo/Program.fs
+++ src/Matrix/Demo/Program.fs
@@ -4,21 +4,16 @@ open MathNet.Numerics.LinearAlgebra
 
 [<EntryPoint>]
 let main argv =
-    let reversedMatrix = matrix [[1.0; 1.0; 0.0]
-                                 [0.0; 1.0; 0.0]
-                                 [0.0; 0.0; 1.0]]
-    let x = vector [1.0; 0.0; 1.0]
+    let reversedMatrix =
+        matrix [ [ 1.0; 1.0; 0.0 ]
+                 [ 0.0; 1.0; 0.0 ]
+                 [ 0.0; 0.0; 1.0 ] ]
+
+    let x = vector [ 1.0; 0.0; 1.0 ]
     let i = 3 - 1
-    let l = Util.multiplyByVector (reversedMatrix, x) // 1.
-    let check = Util.check (l, i)
-    let result =
-        if check.IsSome
-        then
-            let wavedL = Util.setMinusOneAt (x, i) // 2
-            let cuppedL = Util.multiply (l.At i) wavedL // 3
-            let almostE = Util.almostIdentityMatrix i cuppedL // 4
-            let result = Util.finalMultiply almostE reversedMatrix // 5
-            result.ToMatrixString()
-        else "Матрица А с крышкой не обратима!"
-    Console.WriteLine(result)
+
+    let result = Util.perform reversedMatrix x i
+    match result with
+    | Some result -> Console.WriteLine(result.ToMatrixString())
+    | None -> Console.WriteLine("Матрица А с крышкой не обратима!")
     0
--- src/Matrix/Library/Library.fs
+++ src/Matrix/Library/Library.fs
@@ -3,32 +3,40 @@
 open MathNet.Numerics.LinearAlgebra
 
 module Util =
-    let minusOne = -1
-    // 1.
-    let multiplyByVector (A: Matrix<double>, x: Vector<double>) =
-        A * x
-    
-    // 2.0
-    let check (x: Vector<double>, i: int) =
+    let private minusOne = -1
+
+    let private check (x: Vector<double>, i: int) =
         if x.At i = double 0 then None else Some(x)
-    
-    // 2.1
-    let setMinusOneAt (x: Vector<double>, i: int) =
+
+    let private setMinusOneAt (i: int) (x: Vector<double>) =
         let newX = x.Clone()
-        newX.At (i, -1.0)
+        newX.At(i, double minusOne)
         newX
-    
-    // 3.
-    let multiply (multiplier: double) (x: Vector<double>) =
-        (double (-1) / multiplier) * x
-    
-    // 4.
-    let almostIdentityMatrix (column: int) (x:Vector<double>) =
-        let mutable identityMatrix = Matrix<double>.Build.Dense(x.Count, x.Count)
-        (DiagonalMatrix.identity<double> x.Count).CopyTo(identityMatrix)
-        identityMatrix.SetColumn (column, x)
+
+    let private multiply (multiplier: double) (x: Vector<double>) = (double (-1) / multiplier) * x
+
+    let private almostIdentityMatrix (column: int) (x: Vector<double>) =
+        let mutable identityMatrix =
+            Matrix<double>.Build.Dense(x.Count, x.Count)
+
+        (DiagonalMatrix.identity<double> x.Count)
+            .CopyTo(identityMatrix)
+
+        identityMatrix.SetColumn(column, x)
         identityMatrix
-    
-    // 5.
-    let finalMultiply (Q: Matrix<double>) (A: Matrix<double>)=
-        Q * A
+
+    let perform (A: Matrix<double>) (x: Vector<double>) (i: int): Option<Matrix<double>> =
+        //todo: check size corresponding
+        let l = A * x // 1.
+        let check = check (l, i)
+
+        match check with
+        | None -> None
+        | Some l ->
+            let q =
+                x
+                |> setMinusOneAt i // 2.
+                |> multiply (l.At i) // 3.
+                |> almostIdentityMatrix i // 4.
+
+            Some(q * A) // 5.
